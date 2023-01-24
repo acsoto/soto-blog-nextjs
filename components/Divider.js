c9a526270b14bc7dd0f8c90a7b85194e2b62c729
soto <59144459+acsoto@users.noreply.github.com>
2023-01-25 01:57:00 +0800

    modify archive

--- components/Divider.js
+++ components/Divider.js
@@ -4,7 +4,7 @@ const Divider = ({ children }) => {
       <div className="inline-flex w-full items-center justify-center">
         <hr className="my-5 h-px w-full border-0 bg-gray-200 dark:bg-gray-700" />
         <span className="absolute left-1/2 -translate-x-1/2 bg-white px-3 font-medium text-gray-900 dark:bg-gray-900 dark:text-white">
-          {children}
+          <div className="text-xl font-bold text-gray-500">{children}</div>
         </span>
       </div>
     )
--- pages/archive.js
+++ pages/archive.js
@@ -32,16 +32,29 @@ export default function Home({ posts }) {
       <div>
         {Array.from(timeMap.keys()).map((year) => {
           return (
-            <div key={year} className="mx-auto flex flex-col">
-              <Divider>
-                <span className="text-xl font-bold text-gray-500 dark:text-opacity-50">{year}</span>
-              </Divider>
-              {Array.from(timeMap.get(year).keys()).map((month) => (
-                <div key={month} className="container my-3 rounded-xl p-5">
-                  <div>
-                    <div className="mb-2 text-2xl font-bold text-gray-600 dark:text-opacity-50 ">
+            <>
+              <Divider>{year}</Divider>
+              <ol key={year} className="relative border-l border-gray-200 dark:border-gray-700">
+                {Array.from(timeMap.get(year).keys()).map((month) => (
+                  <li key={month} className="mb-10 ml-6">
+                    <span className="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-soto-100 ring-8 ring-white dark:ring-gray-900">
+                      <svg
+                        aria-hidden="true"
+                        className="h-3 w-3 text-soto-200"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                          clipRule="evenodd"
+                        ></path>
+                      </svg>
+                    </span>
+                    <h3 className="mb-1 flex items-center text-lg font-semibold text-gray-700 dark:text-gray-300">
                       {month}
-                    </div>
+                    </h3>
                     <div>
                       {timeMap
                         .get(year)
@@ -64,10 +77,10 @@ export default function Home({ posts }) {
                           )
                         })}
                     </div>
-                  </div>
-                </div>
-              ))}
-            </div>
+                  </li>
+                ))}
+              </ol>
+            </>
           )
         })}
       </div>
