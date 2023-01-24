3441c744be6bbff9ee3b9f05eadc3f64c435d727
soto <59144459+acsoto@users.noreply.github.com>
2023-01-25 02:21:09 +0800

    edit social media icons

--- components/Greetings.js
+++ components/Greetings.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Typed from 'typed.js'
 import Link from '@/components/Link'
+import SocialIcon from '@/components/social-icons'
+import siteMetadata from '@/data/siteMetadata'
 const Greetings = () => {
   // Create reference to store the DOM element containing the animation
   const el = React.useRef(null)
@@ -43,6 +45,8 @@ const Greetings = () => {
         <br />
         SOTO is a convenient way to refer to me on social media.
         <br />
+        This is a place where I share my life, thoughts, and other stuff that might be interesting.
+        <br />
         <br />
         I am now living in China 🇨🇳 and am a senior at Beihang University, studying computer science
         and technology, and I plan to go abroad next year for my master's degree.
@@ -72,7 +76,15 @@ const Greetings = () => {
           {/*</Link>*/}
         </div>
         <br />
-        This is a place where I share my life, thoughts, and other stuff that might be interesting.
+        Happy reading!
+        <div className="flex flex-row gap-2">
+          <span>Contact me:</span>
+          <SocialIcon kind="instagram" href={siteMetadata.instagram} size="6" />
+          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size="6" />
+          <SocialIcon kind="github" href={siteMetadata.github} size="6" />
+          <span>RSS feed:</span>
+          <SocialIcon kind="rss" href="/feed.xml" size="6" />
+        </div>
       </p>
     </>
   )
--- components/social-icons/index.js
+++ components/social-icons/index.js
@@ -4,6 +4,8 @@ import Facebook from './facebook.svg'
 import Youtube from './youtube.svg'
 import Linkedin from './linkedin.svg'
 import Twitter from './twitter.svg'
+import Instagram from './instagram.svg'
+import Rss from './rss.svg'
 
 // Icons taken from: https://simpleicons.org/
 
@@ -14,6 +16,8 @@ const components = {
   youtube: Youtube,
   linkedin: Linkedin,
   twitter: Twitter,
+  instagram: Instagram,
+  rss: Rss,
 }
 
 const SocialIcon = ({ kind, href, size = 8 }) => {
