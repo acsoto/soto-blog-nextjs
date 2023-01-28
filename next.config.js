9715bb95dfe0d7ff1eec64e656a2388a4effe5d4
soto <59144459+acsoto@users.noreply.github.com>
2023-01-28 21:09:38 +0800

    disable ContentSecurityPolicy

--- next.config.js
+++ next.config.js
@@ -16,10 +16,10 @@ const ContentSecurityPolicy = `
 
 const securityHeaders = [
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
-  {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\n/g, ''),
-  },
+  // {
+  //   key: 'Content-Security-Policy',
+  //   value: ContentSecurityPolicy.replace(/\n/g, ''),
+  // },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
   {
     key: 'Referrer-Policy',
