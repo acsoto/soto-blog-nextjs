e722852e2c0f205eb416890238682fa42a813b31
soto <59144459+acsoto@users.noreply.github.com>
2023-02-07 16:42:14 +0800

    modify GitHubCard.tsx

--- components/GitHubCard.tsx
+++ components/GitHubCard.tsx
@@ -7,11 +7,12 @@ const GitHubCard = ({ repo }: { repo: string }) => {
   return (
     <Link href={link}>
       <Image
-        className="overflow-hidden rounded-xl shadow-2xl hover:scale-105 lg:w-2/3"
+        className="overflow-hidden rounded-xl shadow-2xl hover:scale-105 lg:w-1/2"
         src={img}
         alt={repo}
         width={1200}
         height={600}
+        quality={100}
       />
     </Link>
   )
