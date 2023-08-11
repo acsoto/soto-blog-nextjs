d6f4731cd47bbe2cdfc65037c111eb117dde5342
soto <59144459+acsoto@users.noreply.github.com>
2023-08-12 01:25:24 +0800

    remove index page recent blogs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,13 @@
-import Link from '@/components/Link'
 import { PageSeo } from '@/components/SEO'
 import { siteMetadata } from '@/data/siteMetadata'
-import { getAllFilesFrontMatter } from '@/lib/mdx'
 
-import PostCard from '@/components/PostCard'
 import Greetings from '@/components/Greetings'
-import Divider from '@/components/Divider'
-import { FrontMatter } from '@/types/md'
-import { getImgProps } from '@/lib/get-img-props'
 
-const MAX_DISPLAY = 6
-
-export async function getStaticProps() {
-  const posts: FrontMatter[] = await getAllFilesFrontMatter('blog')
-  const showingPosts = posts.slice(0, MAX_DISPLAY)
-
-  for (const post of showingPosts) {
-    post.imgProps = await getImgProps(post.image)
-  }
-
-  return { props: { showingPosts } }
-}
-
-export default function Home({ showingPosts }) {
+export default function Home() {
   return (
     <>
       <PageSeo title={siteMetadata.title} description={siteMetadata.description} />
       <Greetings />
-      <Divider />
-      <ul className={'grid gap-6 lg:grid-cols-2'} style={{ listStyle: `none` }}>
-        {showingPosts.map((post) => {
-          const { slug } = post
-          return (
-            <li key={slug}>
-              <PostCard post={post} />
-            </li>
-          )
-        })}
-      </ul>
-      <Divider className="mt-4">
-        <Link href="/archive" className="text-soto-100 hover:text-soto-200" aria-label="More">
-          More
-        </Link>
-      </Divider>
     </>
   )
 }
